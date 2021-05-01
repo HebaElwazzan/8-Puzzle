cf2a61d1a511c2d23cbcfc403ba085d00fb0afcf
Youssef Nawar <youssef.nawar10@yahoo.com>
2021-05-02 01:16:28 +0200

    no message

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Rowan is 7omara")
+goalState = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+depth = 0
+nodesVisited = 0
+nodesExpanded = 0
+
+
+class GameState:
+    def __init__(self, parent, move):
+        self.parent = parent
+        self.move = move
+        
